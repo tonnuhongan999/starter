4b477be025e530597ee6ec691144f2d9827b93dd
Nowsin Anzum <nowsin4408@gmail.com>
2026-08-24 20:54:33 +0000

    feat:added a message

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,6 +2,6 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world from the team's Sprint 1 project skeleton");
+        System.out.println("Hello world from the team's Sprint 1 project skeleton. I'm the second partner!");
     }
 }
